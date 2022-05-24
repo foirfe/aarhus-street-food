cb7a87a20b9f374ed7385e63ff6f0c6d46db7e40
Stefanos-Langkilde <eaastkl@students.eaaa.dk>
2022-05-24 23:45:54 +0200

    discover næsten færdig

--- src/components/DiscoverCheckbox.js
+++ src/components/DiscoverCheckbox.js
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 import IconParkOutlineChili from "../CookingIcons/IconParkOutlineChili";
 import PhCookingPotBold from "../CookingIcons/PhCookingPotBold";
 import MdiChiliOffOutline from "../CookingIcons/MdiChiliOffOutline";
-import IconParkOutlineFrenchFries from "../CookingIcons/IconParkOutlineFrenchFries";
 import IconParkOutlineCakeFive from "../CookingIcons/IconParkOutlineCakeFive";
 import MdiFruitGrapesOutline from "../CookingIcons/MdiFruitGrapesOutline";
 import IconParkOutlineCarrot from "../CookingIcons/IconParkOutlineCarrot";
+import EpChicken from "../CookingIcons/EpChicken";
 
 export default function DiscoverCheckbox() {
    const [errormessage, setErrorMessage] = useState("");
@@ -41,37 +41,92 @@ export default function DiscoverCheckbox() {
       <div>
          <form onSubmit={handleSubmit}>
             <div className="checkboxes">
-               <label>
-                  <input type="checkbox" value="spicy" onChange={e => setSpicy(e.target.checked)} checked={spicy} />
-                  <IconParkOutlineChili />
-               </label>
-               <label>
-                  <input type="checkbox" value="mildt" onChange={e => setMildt(e.target.checked)} checked={mildt} />
-                  <MdiChiliOffOutline />
-               </label>
-               <label>
-                  <input type="checkbox" value="krydret" onChange={e => setKrydret(e.target.checked)} checked={krydret} />
-                  <PhCookingPotBold />
-               </label>
-               <label>
-                  <input type="checkbox" value="friture" onChange={e => setFriture(e.target.checked)} checked={friture} />
-                  <IconParkOutlineFrenchFries />
-               </label>
-               <label>
-                  <input type="checkbox" value="sodt" onChange={e => setSodt(e.target.checked)} checked={sodt} />
-                  <IconParkOutlineCakeFive />
-               </label>
-               <label>
-                  <input type="checkbox" value="smasulten" onChange={e => setSmasulten(e.target.checked)} checked={smasulten} />
-                  <MdiFruitGrapesOutline />
-               </label>
-               <label>
-                  <input type="checkbox" value="plantebaseret" onChange={e => setPlantebaseret(e.target.checked)} checked={plantebaseret} />
-                  <IconParkOutlineCarrot />
-               </label>
-               <button>Udforsk</button>
-               <p>{errormessage}</p>
+               <div className="row1">
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="spicy" onChange={(e) => setSpicy(e.target.checked)} checked={spicy} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <IconParkOutlineChili />
+                           </span>
+                           <span className="span-text">Spicy</span>
+                        </div>
+                     </label>
+                  </div>
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="mildt" onChange={(e) => setMildt(e.target.checked)} checked={mildt} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <MdiChiliOffOutline />
+                           </span>
+                           <span className="span-text">Sarte Smagsløg</span>
+                        </div>
+                     </label>
+                  </div>
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="krydret" onChange={(e) => setKrydret(e.target.checked)} checked={krydret} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <PhCookingPotBold />
+                           </span>
+                           <span className="span-text">Krydret</span>
+                        </div>
+                     </label>
+                  </div>
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="friture" onChange={(e) => setFriture(e.target.checked)} checked={friture} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <EpChicken />
+                           </span>
+                           <span className="span-text">Friture</span>
+                        </div>
+                     </label>
+                  </div>
+               </div>{" "}
+               {/* row 1 slut */}
+               <div className="row2">
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="sodt" onChange={(e) => setSodt(e.target.checked)} checked={sodt} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <IconParkOutlineCakeFive />
+                           </span>
+                           <span className="span-text">Sødt</span>
+                        </div>
+                     </label>
+                  </div>
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="smasulten" onChange={(e) => setSmasulten(e.target.checked)} checked={smasulten} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <MdiFruitGrapesOutline />
+                           </span>
+                           <span className="span-text">Små Sulten</span>
+                        </div>
+                     </label>
+                  </div>
+                  <div className="label-box">
+                     <label>
+                        <input type="checkbox" value="plantebaseret" onChange={(e) => setPlantebaseret(e.target.checked)} checked={plantebaseret} />
+                        <div className="span-icon-box">
+                           <span className="span-icon">
+                              <IconParkOutlineCarrot />
+                           </span>
+                           <span className="span-text">Plantebaseret</span>
+                        </div>
+                     </label>
+                  </div>
+               </div>{" "}
+               {/* row 2 slut */}
             </div>
+            <button>SE DINE RETTER</button>
+            <p>{errormessage}</p>
          </form>
       </div>
    );
--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -6,7 +6,9 @@ export default function DiscoverPage() {
          <h1>Discover</h1>
          <img src="" alt="Århus street food" />
          <h3>Vælg nogle smage du kan lide, og få nogle forslag til din ynglingsret.</h3>
-         <DiscoverCheckbox />
+         <div className="checkbox">
+            <DiscoverCheckbox />
+         </div>
       </div>
    );
 }
