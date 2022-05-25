74c399bba40a21246209d64641e7f4d20eb4e4ff
Stefan Langkilde <eaastkl@students.eaaa.dk>
2022-05-25 13:27:41 +0200

    discover logo added

--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -1,10 +1,11 @@
 import DiscoverCheckbox from "../components/DiscoverCheckbox";
+import Logo from "../img/Logo.png";
 
 export default function DiscoverPage() {
    return (
       <div className="discoverpage">
          <h1>Discover</h1>
-         <img src="" alt="Århus street food" />
+         <img src={Logo} alt="Århus street food" />
          <h3>Vælg nogle smage du kan lide, og få nogle forslag til din ynglingsret.</h3>
          <div className="checkbox">
             <DiscoverCheckbox />
