4b55995ec08011dbebf2a0d9630742e71850bb57
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-05-27 11:12:40 +0200

    Search, styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,14 @@ import Logo from "../img/Logo.png";
 export default function Footer(){
     return(
         <div className="footer">
+            <a href="/">
         <img src={Logo} alt="Aarhus Street Food"/>
+        </a>
         <div className="footerinfo">
-        <p>Adresse: Ny Banegårdsgade 46, 8000 Aarhus C</p>
-        <p>Åbningstider</p>
-        <p>Mandag-Søndag 11.30 - 21.00</p>
+        <p className="infoitalic">Adresse:</p>
+        <p className="infobold"> Ny Banegårdsgade 46, 8000 Aarhus C</p>
+        <p className="infoitalic">Åbningstider:</p>
+        <p className="infobold">Mandag-Søndag 11.30 - 21.00</p>
         </div>
         </div>
     )
