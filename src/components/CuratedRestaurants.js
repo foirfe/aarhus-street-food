e6f159ecbef1e5aa3698eb452eb76666296d3cbf
Stefanos-Langkilde <eaastkl@students.eaaa.dk>
2022-05-29 20:40:38 +0200

    forberedelse til swipe
    Co-authored-by: Daníel Freyr <foirfe@users.noreply.github.com>

--- src/components/CuratedRestaurants.js
+++ src/components/CuratedRestaurants.js
@@ -1,66 +1,70 @@
-import { useEffect, useState } from "react"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { useNavigate } from "react-router-dom";
 
+export default function CuratedRestaurants() {
+   const navigate = useNavigate();
+   const [curatedPlaces, setCuratedPlaces] = useState([]);
+   useEffect(() => {
+      async function getData() {
+         const response = await fetch("./data/da_curatedplaces.json");
+         const data = await response.json();
+         setCuratedPlaces(data);
+         console.log(curatedPlaces);
+      }
+      getData();
+   }, []);
 
-export default function CuratedRestaurants()
-{
-  const navigate = useNavigate()
-    const [curatedPlaces, setCuratedPlaces] = useState([]);
-    useEffect(()=>{
-        async function getData(){
-            const response = await fetch("./data/da_curatedplaces.json");
-            const data = await response.json();
-            setCuratedPlaces(data);
-            console.log(curatedPlaces);
-        }
-        getData();
-    },[])
-
-function handleClick(event){
-  const id = event.currentTarget.id;
-  navigate(`/restaurant/${curatedPlaces[id]?.slug}`)
-}
-    return(
-        <div>            
-        <Swiper
-        spaceBetween={20}
-        slidesPerView={1.9}
-        onSlideChange={() => console.log('slide change')}
-        centeredSlides= {true}
-        onSwiper={(swiper) => console.log(swiper)}
-        loop={true}
-        slideToClickedSlide={true} 
-        preventClicks={false} 
-        preventClicksPropagation={false}
-        >
-     
-              <SwiperSlide id={0} onClick={handleClick} ><div className="curatedplace" >
+   function handleClick(event) {
+      const id = event.currentTarget.id;
+      navigate(`/restaurant/${curatedPlaces[id]?.slug}`);
+   }
+   return (
+      <div>
+         <Swiper
+            spaceBetween={20}
+            slidesPerView={1.9}
+            onSlideChange={() => console.log("slide change")}
+            centeredSlides={true}
+            onSwiper={(swiper) => console.log(swiper)}
+            loop={true}
+            slideToClickedSlide={true}
+            preventClicks={false}
+            preventClicksPropagation={false}
+         >
+            <SwiperSlide id={0} onDoubleClick={handleClick}>
+               <div className="curatedplace">
                   <h2>{curatedPlaces[0]?.name}</h2>
-                  <img src={curatedPlaces[0]?.image_url} alt={curatedPlaces[0]?.name}/>
-                </div></SwiperSlide>
-        <SwiperSlide id={1} onClick={handleClick}><div className="curatedplace">
+                  <img src={curatedPlaces[0]?.image_url} alt={curatedPlaces[0]?.name} />
+               </div>
+            </SwiperSlide>
+            <SwiperSlide id={1} onDoubleClick={handleClick}>
+               <div className="curatedplace">
                   <h2>{curatedPlaces[1]?.name}</h2>
-                  <img src={curatedPlaces[1]?.image_url} alt={curatedPlaces[1]?.name}/>
-                </div></SwiperSlide>
-            <SwiperSlide id={2} onClick={handleClick}>
-            <div className="curatedplace">
+                  <img src={curatedPlaces[1]?.image_url} alt={curatedPlaces[1]?.name} />
+               </div>
+            </SwiperSlide>
+            <SwiperSlide id={2} onDoubleClick={handleClick}>
+               <div className="curatedplace">
                   <h2>{curatedPlaces[2]?.name}</h2>
-                  <img src={curatedPlaces[2]?.image_url} alt={curatedPlaces[2]?.name}/>
-                </div></SwiperSlide>
-            <SwiperSlide id={3} onClick={handleClick}>
-            <div className="curatedplace">
+                  <img src={curatedPlaces[2]?.image_url} alt={curatedPlaces[2]?.name} />
+               </div>
+            </SwiperSlide>
+            <SwiperSlide id={3} onDoubleClick={handleClick}>
+               <div className="curatedplace">
                   <h2>{curatedPlaces[3]?.name}</h2>
-                  <img src={curatedPlaces[3]?.image_url} alt={curatedPlaces[3]?.name}/>
-                </div></SwiperSlide>
-            <SwiperSlide id={4} onClick={handleClick}>
-            <div className="curatedplace">
+                  <img src={curatedPlaces[3]?.image_url} alt={curatedPlaces[3]?.name} />
+               </div>
+            </SwiperSlide>
+            <SwiperSlide id={4} onDoubleClick={handleClick}>
+               <div className="curatedplace">
                   <h2>{curatedPlaces[4]?.name}</h2>
-                  <img src={curatedPlaces[4]?.image_url} alt={curatedPlaces[4]?.name}/>
-                </div></SwiperSlide>
-        ...
-      </Swiper>
+                  <img src={curatedPlaces[4]?.image_url} alt={curatedPlaces[4]?.name} />
+               </div>
+            </SwiperSlide>
+            ...
+         </Swiper>
       </div>
-    )
+   );
 }
--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,37 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function ResultPage() {
-   const [taste, setTaste] = useState([]);
-   
-   
-   
-
    useEffect(() => {
       async function getData() {
          const response = await fetch("./data/da_menu.json");
          const data = await response.json();
-         const taste = data.filter(key => localStorage.getItem(key));
-         console.log(taste);
-         if (taste) { 
-            console.log("this exists");
-         } else{console.log("does not exist");}
          console.log(data);
-         setTaste()
       }
+      /* function getFilters() {
+
+      } */
+
+      //const filters = getFilters();
+      // const results = data.filter(includes filters kommer her)
+      // UseState bliver sat her
       getData();
-   }, [taste]);
-
-   
-
+   }, []);
 
    return (
       <div className="resultpage">
          <h1>Discover menu</h1>
          <p>Scroll igennem og opdag nye retter</p>
-         <div> 
-                     
-         </div>
+         <div></div>
       </div>
    );
-
 }
