f04773cf41e1a0cbd853b6c523a3256a7fd4b169
Louimer <louise.merfeldt@hotmail.com>
2022-06-01 10:45:35 +0200

    ContactPage styling - done

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -15,18 +15,20 @@ export default function ContactPage() {
           <div className="contactpage-s2-text">
             <h3>HAR DU GLEMT NOGET?</h3>
             <p>
-              Kontakt Den Blå Bar ved indgangen - eller send os en mail på
-              kontakt@aarhusstreetfood.com
+              Så kontakt Den Blå Bar ved indgangen - eller send os en mail hurtigst muligt på <br></br>
+              <a href="mailto: kontakt@aarhusstreetfood.com">  kontakt@aarhusstreetfood.com.</a>
             </p>
 
             <h3>BORDBOOKING</h3>
             <p>
               Vi har bygget det lækreste nye område Grøn Stue med plads til 150
-              siddende gæster. Her tager vi imod bordbestillinger - på minimum
-              16 personer - alle ugens dage.<br></br>
+              siddende gæster. Her tager vi imod bordbestillinger, på minimum
+              16 personer - alle ugens dage.
               Er I færre, finder I bare selv et par langborde i byens bedste
               Garage. Send din forspørgsel på booking til
-              booking@aarhusstreetfood.com.
+              <br></br>
+              <a href="mailto: booking@aarhusstreetfood.com">  booking@aarhusstreetfood.com.</a>
+            
             </p>
 
             <h3>FIND OS HER</h3>
